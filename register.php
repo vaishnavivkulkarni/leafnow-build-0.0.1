bf2c318b696910e9dfaae74dbee355e6e9f43c90
vaishnavivkulkarni <87668034+vaishnavivkulkarni@users.noreply.github.com>
2022-06-02 22:06:52 +0530

    Update register.php

--- register.php
+++ register.php
@@ -155,11 +155,7 @@ mysqli_close($conn);
       <label for="inputCity">City</label>
       <input type="text" class="form-control" id="inputCity">
     </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
+   
       </select>
     </div>
     <div class="form-group col-md-2">
