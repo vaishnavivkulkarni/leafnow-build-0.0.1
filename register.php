3366a63bb39e929395df77ada9862ea398c462fe
vaishnavivkulkarni <87668034+vaishnavivkulkarni@users.noreply.github.com>
2022-06-02 22:05:41 +0530

    Update register.php

--- register.php
+++ register.php
@@ -102,7 +102,7 @@ mysqli_close($conn);
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>PHP login system!</title>
+    <title>Leaf Now</title>
   </head>
   <body>
       
